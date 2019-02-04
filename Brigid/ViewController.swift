75c3b8faddf18360a73c3994731b5632bc39412a
Stephen Walsh <sfwalshappdev@gmail.com>
2019-02-04 10:27:28 -0800

    Initial Commit

---
+++ Brigid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brigid
+//
+//  Created by Stephen Work on 04/02/2019.
+//  Copyright © 2019 Stephen Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
